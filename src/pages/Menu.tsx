ffd754ab724c8de006722034346123bc84d864f3
r̷̡͚̰̖̱̟̤̤̙͔͎̰̤̲̝̤̄͒̂͐͆̆͗̾͐̇̑̇̑̓̇̔̑̈̓͌̉̒̕͘̕͝j̸̡̭̺̹̬̲̙͉̰͇̫͉̝̺̹͈̬̞̋̂͒̅͋̔̎̇̂̐̿̊͋̀͆̄̈́̂̆̇͆̈́̋̾̊̚̚̚͜͠ͅ <144422158+Mrgumball2004@users.noreply.github.com>
2025-02-11 01:16:06 +0800

    Added home/details page route

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -19,6 +19,7 @@ import {
   import { Redirect, Route } from 'react-router';
   import Home from './Home';
   import About from './About';
+  import Details from './Details';
 
   const Template: React.FC = () => {
     const path = [
@@ -59,11 +60,12 @@ import {
                 
                 <IonRouterOutlet id="main">
                  <Route exact path="/it35-lab/app/home" component={Home} />
+                 <Route exact path="/it35-lab/app/home/details" component={Details} />
                  <Route exact path="/it35-lab/app/about" component={About} />
                     <Route exact path="/it35-lab/app">
                         <Redirect to="/it35-lab/app/home"/>
                     </Route>
-                    
+
                 </IonRouterOutlet>
             </IonSplitPane>
         </IonPage>
