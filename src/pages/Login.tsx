5014b62d8a66a7f1d89e71e07789381b7d3b716e
Mrgumball2004 <arjie1419@gmail.com>
2025-02-24 14:43:18 +0800

    added password and user name in Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,13 +10,40 @@ import {
       useIonRouter
   } from '@ionic/react';
   
+  import { IonInput, IonItem, IonList } from '@ionic/react';
+  import { IonInput, IonInputPasswordToggle } from '@ionic/react';
   const Login: React.FC = () => {
     const navigation = useIonRouter();
     const doLogin = () => {
         navigation.push('/it35-lab/app','forward','replace');
+        
     }
 
+    function Example() {
+      const [inputModel, setInputModel] = useState('');
+      const ionInputEl = useRef<HTMLIonInputElement>(null);
+    
+      const onInput = (event: Event) => {
+        const value = (event.target as HTMLIonInputElement).value as string;
+    
+        // Removes non alphanumeric characters
+        const filteredValue = value.replace(/[^a-zA-Z0-9]+/g, '');
+    
+        /**
+         * Update both the state variable and
+         * the component to keep them in sync.
+         */
+        setInputModel(filteredValue);
+    
+        const inputCmp = ionInputEl.current;
+        if (inputCmp !== null) {
+          inputCmp.value = filteredValue;
+        }
+      }
+
+    
     return (
+
 
       <IonPage>
         <IonHeader>
@@ -32,6 +59,17 @@ import {
           <IonButton onClick={() => doLogin()} expand="full">
               Login
           </IonButton>
+
+          <IonInput type="password" label="Password" value="NeverGonnaGiveYouUp">
+      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+    </IonInput>
+
+    <IonList>
+      <IonItem>
+        <IonInput label="Alphanumeric Characters" value={inputModel} onIonInput={onInput} ref={ionInputEl}></IonInput>
+      </IonItem>
+    </IonList>
+
       </IonContent>
     </IonPage>
     );
