13047df9812227e23c7642ddce774f9d46296ec2
r̷̡͚̰̖̱̟̤̤̙͔͎̰̤̲̝̤̄͒̂͐͆̆͗̾͐̇̑̇̑̓̇̔̑̈̓͌̉̒̕͘̕͝j̸̡̭̺̹̬̲̙͉̰͇̫͉̝̺̹͈̬̞̋̂͒̅͋̔̎̇̂̐̿̊͋̀͆̄̈́̂̆̇͆̈́̋̾̊̚̚̚͜͠ͅ <144422158+Mrgumball2004@users.noreply.github.com>
2025-03-07 11:51:02 +0800

    Add some customization on the profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,6 @@
 import { 
   IonButtons,
+  IonButton,
   IonContent, 
   IonHeader, 
   IonMenuButton, 
@@ -34,37 +35,38 @@ const Profile: React.FC = () => {
           <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
+      
       <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-            flexDirection: 'column',
-            padding: '20px',
-            backgroundImage: 'url(https://www.example.com/background.jpg)', // Add a background image
-            backgroundSize: 'cover',
-            backgroundPosition: 'center'
-          }}
-        >
-          <IonCard style={{ width: '100%', maxWidth: '500px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }}>
-            <img alt="Profile" src={profileGif} style={{ display: 'block', borderRadius: '50%', width: '300px', margin: '20px auto' }} />
+        <div className='whole-ion-content'>
+          <IonCard className="profile-card">
+            <img alt="Profile" src={profileGif} className="profile-border" />
             
-            <IonCardHeader>
-              <IonCardTitle style={{ textAlign: 'center' }}>Arj Escape</IonCardTitle>
-              <IonCardSubtitle style={{ textAlign: 'center' }}>3rd Year Student</IonCardSubtitle>
+            <IonCardHeader className="txt-center-profile">
+              <IonCardTitle>Arj Escape</IonCardTitle>
+              <IonCardSubtitle>3rd Year Student</IonCardSubtitle>
             </IonCardHeader>
-            <IonCardContent style={{ textAlign: 'center' }}>
-              <p>Passionate developer with experience in building web and mobile applications.</p>
+
+            <IonCardContent>
+              <p className="txt-center-profile">Passionate Student with average experience in building web and mobile applications.</p>
               <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-                <IonIcon icon={logoFacebook} style={{ ...iconStyle }} className="hover-icon-facebook" />
-                <IonIcon icon={logoInstagram} style={{ ...iconStyle }} className="hover-icon-instagram" />
-                <IonIcon icon={logoGithub} style={{ ...iconStyle }} className="hover-icon-github" />
+                <IonIcon icon={logoFacebook} style={{ ...iconStyle }} className="hover-icon-facebook" onClick={() => window.open('https://www.facebook.com/Arj.escaped')} />
+                <IonIcon icon={logoInstagram} style={{ ...iconStyle }} className="hover-icon-instagram" onClick={() => window.open('https://www.instagram.com/rjxcsxs')} />
+                <IonIcon icon={logoGithub} style={{ ...iconStyle }} className="hover-icon-github" onClick={() => window.open('https://github.com/Mrgumball2004')} />
               </div>
             </IonCardContent>
           </IonCard>
-        </div>
+
+          <div>
+          <IonCard className='profile-description'>
+            <IonCardHeader>
+              <IonCardTitle className="txt-center-profile">About Me</IonCardTitle>
+              <IonCardSubtitle>To be edit</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>My works as an artist</IonCardContent>
+            <IonButton style={{ display: 'block', margin: '0 auto' }} fill="clear" >Show output</IonButton>
+          </IonCard>
+          </div>
+        </div> 
       </IonContent>
     </IonPage>
   );
