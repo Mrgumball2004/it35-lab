bfe6d4f1520700716667a5e21a66927001f092a5
Mrgumball2004 <arjie1419@gmail.com>
2025-04-21 09:46:58 +0000

    Updates

--- assets/status-tap-legacy-CU0_AcIk.js
+++ assets/status-tap-legacy-CU0_AcIk.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DnTZ7XCs.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-Dxeoe6BV.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
