89606642dfc45fb4963dd7fa35719ad591108991
Mrgumball2004 <arjie1419@gmail.com>
2025-04-21 10:29:32 +0000

    added Feedcontainer and made some changes in the feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,97 +1,28 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
+  IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar 
+} from '@ionic/react';
+import FeedContainer from '../../components/FeedContainer';
 
-  import React from 'react';
-  import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
-
-  
-  const Feed: React.FC = () => {
-    return (
-
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-
-        <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>IT35</IonCardTitle>
-        <IonCardSubtitle>Application Development & Emerging Tech</IonCardSubtitle>
-      </IonCardHeader>
-
-      <IonCardContent>Unit:3</IonCardContent>
-
-      <IonButton fill="clear">Show output</IonButton>
-    </IonCard>
-
-
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>IT38</IonCardTitle>
-        <IonCardSubtitle>Enterprise System</IonCardSubtitle>
-      </IonCardHeader>
-
-      <IonCardContent>Unit:3</IonCardContent>
-
-      <IonButton fill="clear">Show output</IonButton>
-    </IonCard>
-
-
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>IT36</IonCardTitle>
-        <IonCardSubtitle>Information Assurance & Security 2</IonCardSubtitle>
-      </IonCardHeader>
-
-      <IonCardContent>Unit:3</IonCardContent>
-
-      <IonButton fill="clear">Show output</IonButton>
-    </IonCard>
-
-
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>IT39</IonCardTitle>
-        <IonCardSubtitle>System Integration & Architecture 2</IonCardSubtitle>
-      </IonCardHeader>
-
-      <IonCardContent>Unit:3</IonCardContent>
-
-      <IonButton fill="clear">Show output</IonButton>
-    </IonCard>
-
-
-    <IonCard>
-      <IonCardHeader>
-        <IonCardTitle>IT37</IonCardTitle>
-        <IonCardSubtitle>Hardware Implementation Technologies</IonCardSubtitle>
-      </IonCardHeader>
-
-      <IonCardContent>Unit:3</IonCardContent>
-
-      <IonButton fill="clear">Show output</IonButton>
-    </IonCard>
-
-
-        </IonContent>
-      </IonPage>
-
-
-
-    );
-  };
-  
-  export default Feed;
+export const Feed: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <FeedContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
