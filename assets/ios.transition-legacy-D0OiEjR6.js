c52f3f5ab96e31015e7b48f426bee446d348e133
Mrgumball2004 <arjie1419@gmail.com>
2025-04-21 15:59:27 +0000

    Updates

--- assets/ios.transition-legacy-D0OiEjR6.js
+++ assets/ios.transition-legacy-D0OiEjR6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dxeoe6BV.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DVcwl9Q8.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
