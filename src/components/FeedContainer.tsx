cc3f74910f2a10878600300151de5776a67797fd
Mrgumball2004 <arjie1419@gmail.com>
2025-04-24 13:15:57 +0800

    change the text color into white on post

--- src/components/FeedContainer.tsx
+++ src/components/FeedContainer.tsx
@@ -184,7 +184,7 @@ const FeedContainer = () => {
                 </IonCardHeader>
   
                 <IonCardContent>
-                  <IonText style={{ color: 'black' }}>
+                  <IonText style={{ color: 'white' }}>
                     <h1>{post.post_content}</h1>
                   </IonText>
                 </IonCardContent>
